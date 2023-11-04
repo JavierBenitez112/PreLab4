0513f4a41d6005ea3f4020dd15bc48c134602c6e
PeDro0210 <pedroruben0210@gmail.com>
2023-11-03 22:16:24 -0600

    Last Commit De mi Parte

--- src/Model/FileManagers/CSVManager.java
+++ src/Model/FileManagers/CSVManager.java
@@ -1,6 +1,9 @@
 package Model.FileManagers;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,8 +15,10 @@ public class CSVManager implements IFile {
     public void Save(Card card, String HASH) throws  ParserConfigurationException {
         String filePath = "src/model/DB/American.csv";
 
-        try (FileWriter writer = new FileWriter(filePath)) {
-    
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            // Read the existing contents of the file
+
+            // Write the new card and hash to the file
             writer.append(card.Usuario).append(",");
             writer.append(String.valueOf(card.ID)).append(",");
             writer.append(card.PublicKey).append(",");
@@ -23,6 +28,7 @@ public class CSVManager implements IFile {
             writer.append(String.valueOf(card.FechaDeVencimiento)).append(",");
             writer.append(String.valueOf(card.CodigoCVV)).append("\n");
             writer.append(HASH).append("\n");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
