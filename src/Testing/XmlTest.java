73a6697e18e7f2585d5fbe0b3a6690280d3d21b7
JavierCC <cif23079@uvg,edu.gt>
2023-11-03 20:55:48 -0600

    Testing corregido

---
+++ src/Testing/XmlTest.java
@@ -0,0 +1,42 @@
+package Testing;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import Model.FileManagers.XMLManager;
+import Model.BasicObjects.Card;
+
+public class XmlTest {
+    private XMLManager xmlManager;
+    private Card testCard;
+
+    @Before
+    public void setUp() {
+        xmlManager = new XMLManager();
+        testCard = new Card("UsuarioPrueba", 123, "PublicKeyPrueba", 100.0, 3, "1234567890123456", "12/25", "123");
+    }
+
+    @Test
+    public void testSave() {
+        String filePath = "src/Model/DB/"; 
+
+        try {
+            xmlManager.Save(testCard, "HASHPrueba");
+
+           
+            File file = new File(filePath);
+            if (file.exists()) {
+                System.out.println("The file exists at: " + file.getAbsolutePath());
+            } else {
+                System.out.println("The file does not exist at: " + file.getAbsolutePath());
+            }
+            assertTrue("El archivo XML debería existir", file.exists());
+        } catch (ParserConfigurationException e) {
+            fail("Se produjo una excepción: " + e.getMessage());
+        }
+    }
+}
+
+
