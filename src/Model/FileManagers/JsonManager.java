a78a07737509efef10940acd02086968abcc8fd6
JavierBenitez112 <javibenn@gmail.com>
2023-11-03 19:41:04 -0600

    JsonManager creado
    JsonManager Creado y funcional, se hizo un test y lo paso.

--- src/Model/FileManagers/JsonManager.java
+++ src/Model/FileManagers/JsonManager.java
@@ -1,10 +1,10 @@
 package Model.FileManagers;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import Model.BasicObjects.Card;
-
-
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -30,25 +30,7 @@ public class JsonManager implements IFile {
             e.printStackTrace();
         }
     }
-        JSONObject jsonCard = new JSONObject();
-        jsonCard.put("Usuario", card.Usuario);
-        jsonCard.put("ID", card.ID);
-        jsonCard.put("PublicKey", card.PublicKey);
-        jsonCard.put("Monto", card.Monto);
-        jsonCard.put("Cuotas", card.Cuotas);
-        jsonCard.put("NumeroDeTarjeta", card.NumeroDeTarjeta);
-        jsonCard.put("CodigoDeSeguridad", card.CodigoDeSeguridad);
-        jsonCard.put("FechaDeVencimiento", card.FechaDeVencimiento);
-        jsonCard.put("CodigoCVV", card.CodigoCVV);
 
-        try (FileWriter fileWriter = new FileWriter(filename)) {
-            fileWriter.write(jsonCard.toJSONString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
     public Card Load(String filename) {
         JSONParser parser = new JSONParser();
         try {
